28264681dda31010b3151da9d0b43c7c3b1bd5e8
Elias Hölén Hannouch <hannouch@student.chalmers.se>
2020-10-07 13:56:26 +0200

    Added playground folder.

---
+++ playground/Table_temp.py
@@ -0,0 +1,32 @@
+from kivymd.app import MDApp
+from kivymd.uix.datatables import MDDataTable
+from kivy.uix.widget import Widget
+from kivy.metrics import dp
+
+class Table(Widget):
+
+    def __init__(self):
+        self.table = MDDataTable(use_pagination=True,
+            column_data=[
+                ("Name", dp(30)),
+                ("Value", dp(30)),
+                ("Date", dp(30)),
+                ("Label", dp(30)),
+                ("Debit/Credit", dp(30))],
+            row_data=[
+                (f"{i + 1}", f"{(i+1) * 2}", "2020-09-"+f"{i+1}".rjust(2, '0'), "Pastry", "Debit")
+                for i in range(30)])
+
+        self.table.bind(on_row_press=self.on_row_press)
+
+    def on_row_press(self, instance_table, instance_row):
+        print(instance_table, instance_row)
+
+class TableApp(MDApp):
+
+    def build(self):
+        self.datatable = Table()
+        return self.datatable
+
+table = TableApp()
+table.run()
